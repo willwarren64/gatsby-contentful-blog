e539806346f5a717376c0863ceed8c2843723db7
willwarren64 <wwarren@64labs.com>
2019-06-12 09:29:45 -0400

    Introduce Night Mode Theme

--- src/components/layout.js
+++ src/components/layout.js
@@ -2,7 +2,7 @@ import React from "react"
 import { css } from "@emotion/core"
 import { useStaticQuery, Link, graphql } from "gatsby"
 import { rhythm } from "../utils/typography"
-
+import MyComponent from "./toggle-theme"
 export default ({ children }) => {
   const data = useStaticQuery(
       graphql`
@@ -24,17 +24,30 @@ export default ({ children }) => {
         padding-top: ${rhythm(1.5)};
       `}
     >
-      <Link to={`/`}>
-        <h3
-          css={css`
-            margin-bottom: ${rhythm(2)};
-            display: inline-block;
-            font-style: normal;
-          `}
-        >
-          {data.site.siteMetadata.title}
-        </h3>
-      </Link>
+      <div css={css`
+      border: 3px solid;
+      border-color: rgb(125,200,125);
+      border-radius: 10px;
+      padding-left: .5rem;
+      padding-top: .5rem;
+      text-align: center;
+      `}>
+        <Link to={`/`}>
+          <h3
+            css={css`
+              backgroundColor: 'var(--bg)',
+              color: 'var(--textNormal)',
+              transition: 'color 0.2s ease-out, background 0.2s ease-out',
+              margin-bottom: ${rhythm(2)};
+              display: inline-block;
+              font-style: normal;
+            `}
+          >
+            {data.site.siteMetadata.title}
+          </h3>
+        </Link>
+      </div>
+        <MyComponent />
       {children}
     </div>
   )
--- src/utils/typography.js
+++ src/utils/typography.js
@@ -1,5 +1,42 @@
 import Typography from "typography"
 import kirkhamTheme from "typography-theme-kirkham"
+import '../global.css'
+// import Wordpress2016 from 'typography-theme-wordpress-2016'
+
+kirkhamTheme.overrideThemeStyles = () => ({
+    a: {
+        color: 'var(--textLink)',
+    },
+    // gatsby-remark-autolink-headers - don't underline when hidden
+    'a.anchor': {
+        boxShadow: 'none',
+    },
+    // gatsby-remark-autolink-headers - use theme colours for the link icon
+    'a.anchor svg[aria-hidden="true"]': {
+        stroke: 'var(--textLink)',
+    },
+    p: {
+        color: 'var(--textNormal)',
+    },
+    h1: {
+        color: 'var(--textNormal)',
+    },
+    h2: {
+        color: 'var(--textNormal)',
+    },
+    h3: {
+        color: 'var(--textNormal)',
+    },
+    h4: {
+        color: 'var(--textNormal)',
+    },
+    label: {
+        color: 'var(--textNormal)',
+    },
+    hr: {
+        background: 'var(--hr)',
+    },
+})
 
 const typography = new Typography(kirkhamTheme)
 
